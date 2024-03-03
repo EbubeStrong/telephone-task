2bec14f6873f7d284cef955f1f5e637c2a28fd51
Abraham Samuel <ebubesammy567@gmail.com>
2024-03-03 19:30:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+class Telephone{
+    constructor(phoneNumbers, observers){
+      this.phoneNumbers = [];
+      this.observers = [];
+    }
+
+    addPhoneNumber(number){
+      this.phoneNumbers.push(number);
+    }
+
+    removePhoneNumber(number){
+      const index = this.phoneNumbers.indexOf(number);
+      if(index >= 0)
+      {
+        this.phoneNumbers.splice(index, 1);
+      } 
+    }
+
+
+    dialPhoneNumber(number){
+      if(this.phoneNumbers.includes(number)){
+        this.notifyObservers(number);
+      }else{
+        this.notifyObservers(`${number} is not in your contacts`);
+      }
+    }
+   
+    addObserver(observer){
+      this.observers.push(observer);
+    }
+    removeObserver(observer){
+      const index = this.observers.indexOf(observer);
+      if(index !== -1)
+      {
+        this.observers.splice(index,1);
+      }
+    }
+    notifyObservers(phoneNumber){
+      this.observers.forEach(observer => observer.update(phoneNumber));
+    }
+}
+
+
+  class Observer{
+    update(phoneNumber){
+      console.log("Dialing the number", phoneNumber);
+    }
+  }
+  class ObservedUser1 extends Observer{
+    update(phoneNumber){
+      console.log("Dialed", phoneNumber);
+    }
+  }
+  class ObservedUser2 extends Observer{
+    update(phoneNumber){
+      console.log("Now Dialing", phoneNumber);
+    }
+  }
+  const telephone = new Telephone();
+  const observer1 = new ObservedUser1();
+  const observer2 = new ObservedUser2();
+  telephone.addObserver(observer1);
+  telephone.addObserver(observer2);
+  telephone.addPhoneNumber('0712345678');
+  telephone.dialPhoneNumber('0712345678');
+  telephone.removePhoneNumber('0712345678');
+  telephone.dialPhoneNumber('0712345678');
+  telephone.removeObserver(observer1);
